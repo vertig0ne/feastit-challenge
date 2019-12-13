266db9b073ff8fe691bd7295048a43e1473de5d0
Vertig0ne <23177238+vertig0ne@users.noreply.github.com>
2019-12-13 12:25:24 +0000

    suggestions route add

--- controllers/suggestions.controller.js
+++ controllers/suggestions.controller.js
@@ -1,7 +1,28 @@
+const { Client } = require('pg');
+const client = new Client();
+client.connect();
+
 const get = (req, res, next) => {
     try {
-        const message = 'Hello Again';
-        res.json({message});
+        if (!req.query.q) throw new Error('No Query Provided');
+        let query = `SELECT * FROM movies WHERE film ILIKE '%${req.query.q}%'`;
+        if (req.query.sortBy) {
+            switch (req.query.sortBy) {
+            case 'rotten':
+                query += ' ORDER BY rotten_score DESC';
+                break;
+            case 'audience':
+                query += ' ORDER BY audience_score DESC';
+                break;
+            default:
+                throw new Error('Unknown sorting field');
+            }
+        }
+
+        client.query(query, (err, pgres) => {
+            if (err) throw err;
+            res.json(pgres.rows);
+        });
     } catch (err) { next(err); }
 };
 
